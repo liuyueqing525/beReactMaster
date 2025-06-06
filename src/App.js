32269b6bbcc8764b2dc361e731d6b894f0f7f2ac
刘月清 <505876704@qq.com>
2025-06-06 16:11:35 +0800

    test1：react state初体验

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import ReactDOM from'react-dom/client';
+import "./App.css";
+import Hello from "./hello";
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function App() {
+  
+  
+  class Count extends React.Component {
+    state = { num: 0 };
+    render() {
+      let { num } = this.state;
+      return (
+        <div>
+          {num}
+          <button onClick={()=>{
+            num++;
+            this.setState({num}) 
+          }}>递加</button>
+        </div>
+      );
+    }
+  } 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <h1>Hello, React!</h1> */}
+        <Hello />
+        <Count />
+        {/* <div>{arr}</div> */}
+        
       </header>
     </div>
   );
